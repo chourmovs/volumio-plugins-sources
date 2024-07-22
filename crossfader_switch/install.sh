225b946e060ec050f6119201741722e114c1fdb7
chourmovs <40670708+chourmovs@users.noreply.github.com>
2024-07-22 10:49:15 +0200

    Delete crossfader_switch directory

--- crossfader_switch/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Installing crossfader switch Dependencies"
-## sudo apt-get update
-# Install the required packages via apt-get
-## sudo apt-get -y install
-
-#required to end the plugin install
-echo "plugininstallend"
